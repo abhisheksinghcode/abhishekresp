295a0ba5bf171f9bddbf466ac8896fc99aa51b1e
Abhishek singh <117381540+abhisheksinghcode@users.noreply.github.com>
2024-12-21 12:46:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PTB8JTNRFH"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PTB8JTNRFH');
+</script>
